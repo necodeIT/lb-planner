f972322e882e27b7483d31f664162ba31e29a81b
RiedleroD <github@riedler.wien>
2024-07-13 16:48:06 +0200

    removed embedded script, modify file instead of overwrite

--- document_services.py
+++ document_services.py
@@ -174,165 +174,6 @@ def parse_params(input_text: str) -> dict[str, dict[str, str | bool | None]]:
     return result
 
 
-SCRIPT = r"""
-
-const funcs = /* JSON data here */;
-
-
-let groups = [];
-
-for (let func of funcs) {
-    let group = groups.find(g => g.groupName === func.group);
-    if (!group) {
-        group = {
-            groupName: func.group,
-            functions: []
-        };
-        groups.push(group);
-    }
-    group.functions.push(func);
-}
-
-const sidebar = document.getElementById('sidebar');
-const content = document.getElementById('content');
-
-let currentFunction = null;
-
-
-function searchFunction() {
-
-    const input = document.getElementById('search').value.toUpperCase();
-    const functions = document.querySelectorAll('.func-name');
-
-    for (let i = 0; i < functions.length; i++) {
-        const textValue = functions[i].textContent || functions[i].innerText;
-        if (funcName(textValue).toUpperCase().includes(input)) {
-            functions[i].style.display = "";
-        } else {
-            functions[i].style.display = "none";
-        }
-    }
-
-    // check for func-group and if it has any children that are visible and if not hide it
-    const groups = document.querySelectorAll('.func-group');
-    for (let group of groups) {
-        let visible = false;
-        for (let child of group.children) {
-            if (!child.classList.contains('func-name')) continue;
-
-            child.style.display !== 'none' ? visible = true : null;
-
-            if (visible) break;
-        }
-
-        visible ? group.style.display = '' : group.style.display = 'none';
-    }
-}
-
-const groupsElement = document.querySelector('#func-groups');
-const detailsPane = document.querySelector('#func-details');
-
-function displayGroupsAndFunctions() {
-    groups.forEach(group => {
-        const groupElement = document.createElement('div');
-        groupElement.classList.add('func-group');
-
-        groupName = document.createElement('h1');
-        groupName.textContent = group.groupName;
-
-        groupElement.appendChild(groupName);
-        groupsElement.appendChild(groupElement);
-
-        group.functions.forEach(func => {
-            const funcElement = document.createElement('p');
-            funcElement.classList.add('func-name');
-            funcElement.textContent = func.function_name;
-            funcElement.onclick = () => {
-                if (currentFunction) {
-                    currentFunction.removeAttribute('active');
-                }
-
-                funcElement.setAttribute('active', '');
-
-                currentFunction = funcElement;
-
-                displayFunctionDetails(func);
-            };
-            groupElement.appendChild(funcElement);
-        });
-    });
-    let bottomSpacer = document.createElement('div');
-    bottomSpacer.style.height = '100px';
-    groupsElement.appendChild(bottomSpacer);
-}
-
-function funcName(fullName) {
-    // remove everything from the third underscore
-    const split = fullName.split('_');
-    return split.slice(3, split.length).join(' ');
-}
-
-function displayFunctionDetails(func) {
-
-    detailsPane.innerHTML = `
-    <h1>${funcName(func.function_name)}</h1>
-    <p id="full-func-name">${func.function_name} <i class="fa-regular fa-copy" id="copy"></i></p>
-    <hr />
-    <p id="func_desc">${func.description}</p>
-    <h2>Capabilities</h2>
-    <hr />
-    <p>${func.capabilities}</p>
-    <h2>Parameters</h2>
-    <hr />
-    ${Object.keys(func.parameters).map(param => `
-    <div class="param">
-        <div class="param-signature">
-            <p class="param-name">${param}</p>
-            <p>${func.parameters[param].type}</p>
-            <p class="param-${func.parameters[param].required ? 'required' : 'optional'}">${func.parameters[param].required ? 'required' : 'optional'}</p>
-        </div>
-        <p class="param-desc">${func.parameters[param].description}</p>
-    </div>
-`).join('')}
-    <h2>Returns${func.returns_multiple ? '<p class="badge" style="  font-size: 0.7rem;">Array</p>' : ""}</h2>
-    <hr />
-    ${Object.keys(func.returns).map(ret => `
-    <div class="value">
-    <div class="value-signature">
-      <p class="value-name">${ret}</p>
-      <p>${func.returns[ret].type}</p>
-    </div>
-    <p class="value-desc">${func.returns[ret].description}</p>
-  </div>
-`).join('')}
-    `;
-
-    let copy = document.getElementById('copy');
-    copy.onclick = () => {
-        navigator.clipboard.writeText(func.function_name);
-    }
-
-    let returnValues = document.querySelectorAll('.value-name');
-    let params = document.querySelectorAll('.param-name');
-
-    for (let value of returnValues){
-        value.addEventListener('click', () => {
-            navigator.clipboard.writeText(value.innerText)
-        });
-    }
-
-    for (let param of params){
-        param.addEventListener('click', () => {
-            navigator.clipboard.writeText(param.innerText)
-        });
-    }
-}
-
-displayGroupsAndFunctions();
-
-document.getElementById('search').addEventListener('keyup', searchFunction);
-"""
-
 if __name__ == "__main__":
     with open("lbplanner/db/services.php", "r") as file:
         content = file.read()
@@ -363,7 +204,16 @@ if __name__ == "__main__":
 
                 complete_info.append(incomplete_info)
 
-        script = SCRIPT.replace("/* JSON data here */", json.dumps(complete_info))
+        declaration = f"const funcs = {json.dumps(complete_info)}"
 
-        with open(f"{sys.argv[1]}/script.js", "w") as output_file:
-            output_file.write(script)
+        script: str
+        with open(f"{sys.argv[1]}/script.js", "r") as f:
+            script = f.read()
+            lines = script.splitlines()
+            for i in range(len(lines)):
+                if lines[i].startswith('const funcs = '):
+                    lines[i] = declaration
+            script = "\n".join(lines)
+        
+        with open(f"{sys.argv[1]}/script.js", "w") as f:
+            f.write(script)
